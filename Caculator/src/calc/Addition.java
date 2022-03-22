9e2f60dfcc5fb190392e7160c7ddc681fc2e3e09
pshukla <pshukla@INDHYDELP204.planit.local>
2022-03-22 16:30:20 +0530

    added files

--- Caculator/src/calc/Addition.java
+++ Caculator/src/calc/Addition.java
@@ -3,15 +3,15 @@ public class Addition
 {
 	public void add(int a,int b)
 	{
-				System.out.println(a+b);
-		
+		System.out.println(a+b);
+
 	}
-      public static void main(String[] args)
-      {
-    	  Addition x= new Addition();
-    	  x.add(5, 6);
-    	  
-    		  
+	public static void main(String[] args)
+	{
+		Addition x= new Addition();
+		x.add(5, 6);
+
+
 
 	}
 
