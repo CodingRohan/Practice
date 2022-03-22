014a1043f10a1e6619fc3bb4688ac0aa85820a48
rbhardwaj <rbhardwaj@INDHYDELP202.planit.local>
2022-03-22 12:20:07 +0530

    Created a project and made addition

---
+++ Caculator/src/calc/Addition.java
@@ -0,0 +1,18 @@
+package calc;
+public class Addition 
+{
+	public void add(int a,int b)
+	{
+				System.out.println(a+b);
+		
+	}
+      public static void main(String[] args)
+      {
+    	  Addition x= new Addition();
+    	  x.add(5, 6);
+    	  
+    		  
+
+	}
+
+}
